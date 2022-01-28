2706adf23c6e27b2c1a2c6e8d1be34252eadee4a
Amalia <amalialorenaferariu@gmail.com>
2022-01-28 17:05:36 +0100

    comic factory

---
+++ database/factories/comicFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Comic;
+use Faker\Generator as Faker;
+
+$factory->define(Comic::class, function (Faker $faker) {
+    return [
+        'title'=>$faker->words(3,true),
+        'author'=>$faker->name(),
+        'release_date'=>$faker->date(),
+        'pages'=>$faker->numberBetween(30, 900),
+    ];
+});
